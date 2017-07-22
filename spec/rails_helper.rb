0336098e0aa018c6becdc4796bc11d21d2817485
jim lesperance <contact@jimlesperance.com>
2017-07-22 18:39:42 +0200

    fix typo

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,7 +18,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
 
-  config.include FactoryGirl::Syntax::Method
+  config.include FactoryGirl::Syntax::Methods
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
