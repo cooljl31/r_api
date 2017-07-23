7827da535cd8081bb5c78270d0210d008f2dedd5
jim lesperance <contact@jimlesperance.com>
2017-07-23 16:12:44 +0200

    move some configuration to a defaults.rb file

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,22 +1,6 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
-included do
-        prefix "api"
-        version "v1", using: :path
-        default_format :json
-        format :json
-        formatter :json, Grape::Formatter::ActiveModelSerializers
-    helpers do
-      def permitted_params
-        @permitted_params ||= declared(params,
-            include_missing: false)
-      end
-
-      def logger
-        Rails.logger
-      end
-    end
-
+  included do
     rescue_from ActiveRecord::RecordNotFound do |exception|
       json_response({ message: exception.message }, :not_found)
     end
