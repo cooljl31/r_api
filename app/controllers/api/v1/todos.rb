db01e911a222195effa9a4e493632df2fb95a934
jim lesperance <contact@jimlesperance.com>
2017-07-23 16:13:17 +0200

    add defaults api configurations

--- app/controllers/api/v1/todos.rb
+++ app/controllers/api/v1/todos.rb
@@ -1,12 +1,20 @@
 module API
   module V1
     class Todos < Grape::API
-      include ExceptionHandler
+      include API::V1::Defaults
 
       resources :todos do
-        desc 'returns all todos'
-        get '', root: :path do
+        desc 'Return all todos'
+        get '', root: :todos do
           Todo.all
+        end
+
+        desc 'Return a Todo'
+        params do
+          requires :id, type: Integer, desc: 'ID of the Todo'
+        end
+        get ':id', root: :todos do
+          Todo.where(id: permitted_params[:id])
         end
       end
     end
