8cca622b40a294166d93eaaa2422cb3faa1880ac
jim lesperance <contact@jimlesperance.com>
2017-07-24 08:29:34 +0200

    the todo_id is not the  right one  to fix

--- app/controllers/api/v1/items.rb
+++ app/controllers/api/v1/items.rb
@@ -8,11 +8,28 @@ module API
           requires :todo_id, type: Integer
         end
 
+        desc 'Return a Item'
         route_param :todo_id do
           resources :items do
             get '', root: :items do
               @todo = Todo.find(permitted_params[:todo_id])
               @todo.items
+            end
+          end
+        end
+      end
+
+      resources :todos do
+        desc 'Create Item'
+        params do
+          requires :name, type: String, desc: 'Item name'
+          requires :done, type: Boolean, desc: 'Item status'
+        end
+        route_param :todo_id do
+          resources :items do
+            post '', root: :items do
+              @todo = Todo.find_by(permitted_params[:todo_id])
+              @todo.items.create!(permitted_params)
             end
           end
         end
